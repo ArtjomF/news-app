d0e9d5645d0f81222c71cee616809951e53da34b
ArtjomF <112515137+ArtjomF@users.noreply.github.com>
2022-11-11 16:59:43 +0200

    Form sumit

--- src/Body/form.js
+++ src/Body/form.js
@@ -1,27 +1,54 @@
+import { useState } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup'
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function FormComponent({ show, handleClose }) {
 
-    const langauges = [
+    const [startDateFrom, setStartDateFrom] = useState(new Date());
+    const [startDateTo, setStartDateTo] = useState(new Date());
+    const dateFormat = "dd-MM-yyyy";
+
+    const languages = [
         { label: 'English', code: 'en' },
         { label: 'Russian', code: 'ru' },
         { label: 'Germany', code: 'de' },
         { label: 'French', code: 'fr' },
         { label: 'Spanish', code: 'sp' }
     ];
+
+    function capitalize(str) {
+        return str[0].toUpperCase() + str.substring(1);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const data = {
+            q: event.target.q.value,
+            from: moment(event.target.from.value, dateFormat).format("YYYY-MM-DDT00:00:00.000"),
+            to: moment(event.target.to.value, dateFormat).format("YYYY-MM-DDT23:59:59:999"),
+            language: event.target.language.value,
+            searchIn: [...event.target.searchIn].filter(input => input.checked).map(input => input.value).join(','),
+        };
+
+        console.log(data);
+    }
+
     return (
         <Offcanvas show={show} onHide={handleClose}>
             <Offcanvas.Header closeButton>
                 <Offcanvas.Title>Search News</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3">
                         <Form.Label>Keywords</Form.Label>
-                        <Form.Control type="text" placeholder="Enter keywords or phrases" />
+                        <Form.Control type="text" name="q" placeholder="Enter keywords or phrases" />
                         <Form.Text className="text-muted">
                             Advanced search is supported here
                         </Form.Text>
@@ -30,9 +57,10 @@ function FormComponent({ show, handleClose }) {
                     {['title', 'description', 'content'].map((type) => (
                         <div key={`${type}`} className="mb-3">
                             <Form.Check
-                                label={type}
-                                name="searchin"
+                                label={capitalize(type)}
+                                name="searchIn"
                                 type="checkbox"
+                                value={type}
                                 id={`inline-${type}-1`}
                             />
                         </div>
@@ -40,16 +68,28 @@ function FormComponent({ show, handleClose }) {
                     <Form.Group className="mb-3">
                         <Form.Label>From - to</Form.Label>
                         <InputGroup className="mb-3">
-                            <Form.Control aria-label="from" />
-                            <Form.Control aria-label="to" />
+                            <DatePicker
+                                className="form-control"
+                                selected={startDateFrom}
+                                onChange={(date) => setStartDateFrom(date)}
+                                name="from"
+                                dateFormat={dateFormat}
+                            />
+                            <DatePicker
+                                className="form-control"
+                                selected={startDateTo}
+                                onChange={(date) => setStartDateTo(date)}
+                                name="to"
+                                dateFormat={dateFormat}
+                            />
                         </InputGroup>
                     </Form.Group>
 
                     <Form.Group className="mb-3">
-                        <Form.Label>Select langauges</Form.Label>
-                        <Form.Select aria-label="Default select example">
-                            {langauges.map((lang) => (
-                                <option>{lang.label}</option>
+                        <Form.Label>Select language</Form.Label>
+                        <Form.Select name="language" aria-label="Default select example">
+                            {languages.map((lang) => (
+                                <option key={lang.code} value={lang.code}>{lang.label}</option>
                             ))}
                         </Form.Select>
                     </Form.Group>
--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,24 +1,16 @@
+import { useState } from 'react';
 import Pagination from 'react-bootstrap/Pagination';
 
 function PaginationComponent() {
+
+  const [page, setPage] = useState(1);
+
   return (
-    <Pagination className='mt-4 float-end'>
-      <Pagination.First />
-      <Pagination.Prev />
-      <Pagination.Item>{1}</Pagination.Item>
-      <Pagination.Ellipsis />
-
-      <Pagination.Item>{10}</Pagination.Item>
-      <Pagination.Item>{11}</Pagination.Item>
-      <Pagination.Item active>{12}</Pagination.Item>
-      <Pagination.Item>{13}</Pagination.Item>
-      <Pagination.Item disabled>{14}</Pagination.Item>
-
-      <Pagination.Ellipsis />
-      <Pagination.Item>{20}</Pagination.Item>
-      <Pagination.Next />
-      <Pagination.Last />
-    </Pagination>
+    <Pagination className="mt-4  justify-content-end">
+      <Pagination.Prev onClick={() => setPage(page - 1)}/>
+     <Pagination.Item disabled>{page} / 140</Pagination.Item>
+     <Pagination.Next onClick={() => setPage(page + 1)}/>
+     </Pagination>
   );
 }
 
