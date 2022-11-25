11b83307fd4a1f984a8300059282cfabed744acf
ArtjomF <112515137+ArtjomF@users.noreply.github.com>
2022-11-25 14:13:27 +0200

    Contact

--- src/Body/Contact.js
+++ src/Body/Contact.js
@@ -1,7 +1,27 @@
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function ContactComponent() {
-    return(
-        <div>Contact</div>
+    return (
+
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="" />
+            <Card.Body>
+                <Card.Title>Hello to everybody!</Card.Title>
+                <Card.Text>
+                    This project was build by React.
+                </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroup.Item>My name is Artjom Filimonov</ListGroup.Item>
+                <ListGroup.Item>Tallinn, Estonia</ListGroup.Item>
+                <ListGroup.Item>artjomf@gmail.com</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+        </Card>
     )
 }
 
--- src/Body/index.js
+++ src/Body/index.js
@@ -6,15 +6,15 @@ import NewsCardComponent from './NewsCard';
 import FormComponent from './Form';
 import { getEverything } from '../Services/apiServices';
 import { useDispatch, useSelector } from 'react-redux';
-import{ setErrorMessage, setSearchParams, setTotalResults } from '../Services/stateService';
-import { Link, useParams } from 'react-router-dom';
+import { setErrorMessage, setSearchParams, setTotalResults } from '../Services/stateService';
+import { useParams } from 'react-router-dom';
 import './News.scss';
 
 function NewsCroupComponent() {
 
     const [show, setShow] = useState(false);
     const [articles, setArticles] = useState([]);
-    
+
 
     const handleShow = () => setShow(true);
     const handleClose = () => setShow(false);
@@ -26,7 +26,7 @@ function NewsCroupComponent() {
     const searchParams = useSelector((state) => state.searchParams);
 
     useEffect(() => {
-        if(lang && searchParams.language !== lang){
+        if (lang && searchParams.language !== lang) {
             dispatch(setSearchParams({
                 ...searchParams,
                 language: lang,
@@ -38,7 +38,7 @@ function NewsCroupComponent() {
                 const response = await getEverything({
                     ...searchParams,
                     q: q || searchParams.q,
-                    });
+                });
                 const responseData = await response.json();
                 if (responseData.status === 'error') {
                     throw responseData;
@@ -57,7 +57,9 @@ function NewsCroupComponent() {
             <Button variant="outline-primary" onClick={handleShow} className="mb-2">
                 Search
             </Button>
-            <Link to="/bitcoin">Bitcoin today</Link>
+            <Button variant="outline-primary"  className="mb-2" href="/bitcoin">
+            Bitcoin today
+            </Button>
             <Row xs={1} md={2} lg={3} className="g-2">
                 {articles.map((article, idx) => (
                     <Col key={idx}>
